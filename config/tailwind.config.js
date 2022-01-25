a08ea71e624b1ff04b262bb5e61a1f791077df84
Annie Walsh <annie@anyonething.co.uk>
2022-01-25 19:57:27 +0000

    adds missing config.js code and time logic

--- config/tailwind.config.js
+++ config/tailwind.config.js
@@ -1,21 +1,54 @@
-const defaultTheme = require('tailwindcss/defaultTheme')
-
 module.exports = {
-  content: [
-    './app/helpers/**/*.rb',
-    './app/javascript/**/*.js',
-    './app/views/**/*'
-  ],
+  purge: {
+    content: ["./public/**/*.html"],
+  },
+  darkMode: false, // or 'media' or 'class'
   theme: {
+    colors: {
+      secondary: "#F4F2ED",
+      black: "black",
+      white: "white",
+    },
+    fontFamily: {
+      "pt-serif": ["PT Serif", "serif"],
+      montserrat: ["Montserrat", "sans-serif"],
+    },
+    backgroundSize: {
+      auto: "auto",
+      cover: "cover",
+      contain: "contain",
+      "100%": "100%",
+    },
     extend: {
-      fontFamily: {
-        sans: ['Inter var', ...defaultTheme.fontFamily.sans],
+      backgroundImage: {
+        underline1: "url('./assets/Underline1.svg')",
+        underline2: "url('./assets/Underline2.svg')",
+        underline3: "url('./assets/Underline3.svg')",
+        underline4: "url('./assets/Underline4.svg')",
+        highlight3: "url('./assets/Highlight3.svg')",
+      },
+      keyframes: {
+        "fade-in-down": {
+          "0%": {
+            opacity: "0",
+            transform: "translateY(-10px)",
+          },
+          "100%": {
+            opacity: "1",
+            transform: "translateY(0)",
+          },
+        },
+      },
+      animation: {
+        "fade-in-down": "fade-in-down 0.5s ease-out",
       },
     },
+  },
+  variants: {
+    extend: {},
   },
   plugins: [
     require('@tailwindcss/forms'),
     require('@tailwindcss/aspect-ratio'),
-    require('@tailwindcss/typography'),
-  ]
-}
+    require('@tailwindcss/typography'),],
+};
